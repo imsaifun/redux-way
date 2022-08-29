727420993264a326827212d63ff5ddd7a8e5db9c
imsaifun <imsaifun@gmail.com>
2022-08-29 19:50:44 +0600

    working

--- pages/[id].js
+++ pages/[id].js
@@ -1,8 +1,19 @@
-import React from 'react';
+import { useRouter } from 'next/router';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import Footer from '../components/layout/Footer';
 import Header from '../components/layout/Header';
+import { fetchVideo } from '../redux/features/video/video';
 
 const VideoDetails = () => {
+
+    const dispatch = useDispatch();
+    const router = useRouter();
+    const { id } = router.query;
+    // console.log(id);
+    useEffect(() => {
+        dispatch(fetchVideo(id))
+    }, [dispatch, id])
     return (
         <>
             <Header />
--- redux/store.js
+++ redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import TagsReducers from "./features/tags/tags"
 import VideosReducers from "./features/videos/videos"
+import VideoReducers from "./features/video/video"
 
 export const store = configureStore({
   reducer: {
     videos: VideosReducers,
+    video: VideoReducers,
     tags: TagsReducers,
   },
 });
