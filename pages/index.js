d8fd3ac60d8f3103690c02d99d7779a2e8af2772
imsaifun <imsaifun@gmail.com>
2022-08-29 15:08:04 +0600

    working

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 
 import Pagination from "../components/elements/Pagination";
+import Tags from "../components/elements/Tags";
 import Footer from "../components/layout/Footer";
 import Header from "../components/layout/Header";
 import Grid from "../components/videos/grid";
@@ -9,6 +10,7 @@ export default function Home() {
     return (
         <>
             <Header />
+            <Tags/>
             <Grid />
             <Pagination />
             <Footer />
